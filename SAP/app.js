ba48607943f1877e15748b066a9bce3660e404f1
tuhin <tuhintowhidul9@gmail.com>
2017-10-03 23:10:10 +0600

    nothing changed

--- SAP/app.js
+++ SAP/app.js
@@ -50,9 +50,9 @@ app.use(passport.session());
 // Express Validator
 app.use(expressValidator({
   errorFormatter: function(param, msg, value) {
-      var namespace = param.split('.')
-      , root    = namespace.shift()
-      , formParam = root;
+      var namespace = param.split('.'),
+      root    = namespace.shift(),
+      formParam = root;
 
     while(namespace.length) {
       formParam += '[' + namespace.shift() + ']';
